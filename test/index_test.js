b3804bb917b1b5ef36ba78209fea950dcabe158a
hal.zhong <zhongnxingdou@gmail.com>
2017-01-18 15:31:28 +0800

    Add test case for $listen()

--- test/index_test.js
+++ test/index_test.js
@@ -15,6 +15,25 @@ function async (done, fn) {
 describe('index', () => {
   Vue.use(VueListen)
 
+  it('should add $listen() to Vue', () => {
+    let vm = new Vue()
+    assert.isFunction(vm.$listen)
+  })
+
+  it('should $listen normal', (done) => {
+    let watcher = sinon.spy()
+    let vm = new Vue({
+      data: {
+        name: 'hal'
+      }
+    })
+
+    vm.$listen('name', watcher)
+    vm.name = 'hal.zhong'
+
+    async(done, () => assert.equal(watcher.called, true))
+  })
+
   it('should invoke listener', (done) => {
     let watcher = sinon.spy()
 
